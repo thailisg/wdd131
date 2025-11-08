23ec60440a6256722b9923a37a1e7c9d3d0ad2df
thailisg <thai.ori.gon.diaz@gmail.com>
2025-11-08 17:34:10 -0300

    temples activity

---
+++ scripts/temples.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.querySelector('#menu-toggle');
+    const navigation = document.querySelector('.navigation');
+
+    menuButton.addEventListener('click', () => {
+        navigation.classList.toggle('active');
+        menuButton.textContent = navigation.classList.contains('active') ? '✖' : '☰';
+    });
+});
